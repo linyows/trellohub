b14aaf49372cffa9aee41d4c6a1c139d20fbeeb5
linyows <linyows@gmail.com>
2014-03-21 21:04:00 +0900

    move scope to instance from static

--- lib/trellohub/form/card.rb
+++ lib/trellohub/form/card.rb
@@ -20,19 +20,6 @@ module Trellohub
           end
         end
 
-        # e.g.
-        # => #<MatchData
-        # "synced_issue: https://github.com/linyows-Z_2/trellohub-foo_aaa-123/issues/127"
-        # 1:"linyows-Z_2/trellohub-foo_aaa-123"
-        # 2:"127">
-        def key_matcher
-          /synced_issue:\shttps?:\/\/.*?\/([\w\-\/]+)\/(?:issues|pulls)\/(\d+)/
-        end
-
-        def card_name_prefix_matcher
-          /^[\w\-]+#\d+\s/
-        end
-
         def included(base)
           base.class_eval do
             attr_accessor(*Trellohub::Form::Card.attributes)
@@ -51,11 +38,24 @@ module Trellohub
         build_issue_attributes_by_card
       end
 
+      # e.g.
+      # => #<MatchData
+      # "synced_issue: https://github.com/linyows-Z_2/trellohub-foo_aaa-123/issues/127"
+      # 1:"linyows-Z_2/trellohub-foo_aaa-123"
+      # 2:"127">
+      def key_matcher
+        /synced_issue:\shttps?:\/\/.*?\/([\w\-\/]+)\/(?:issues|pulls)\/(\d+)/
+      end
+
+      def card_name_prefix_matcher
+        /^[\w\-]+#\d+\s/
+      end
+
       def build_issue_attributes_by_card
-        @issue_title = @origin_card.name.gsub(self.class.card_name_prefix_matcher, '')
+        @issue_title = @origin_card.name.gsub(card_name_prefix_matcher, '')
         @issue_state = @origin_card.closed ? 'closed' : 'open'
 
-        if @origin_card.desc =~ self.class.key_matcher
+        if @origin_card.desc =~ key_matcher
           @issue_repository = $1
           @issue_number = $2
 
