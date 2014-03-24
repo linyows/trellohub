13589fb82d12f4e7942b7e48dcd779d6b43c49a0
linyows <linyows@gmail.com>
2014-03-24 18:36:07 +0900

    add missing @key

--- lib/trellohub/form/card.rb
+++ lib/trellohub/form/card.rb
@@ -59,6 +59,7 @@ module Trellohub
         if @origin_card.desc =~ key_matcher
           @issue_repository = $1
           @issue_number = $2
+          @key = "#{@issue_repository}##{@issue_repository.number}"
 
           repo = Trellohub.repository_by(full_name: @issue_repository)
           @issue_milestone = repo.milestone.title if repo.milestone?
