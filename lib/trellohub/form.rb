5271eea03fed5a19a29bd8559bce89e33920fcef
linyows <linyows@gmail.com>
2014-03-24 18:39:44 +0900

    add diff module

--- lib/trellohub/form.rb
+++ lib/trellohub/form.rb
@@ -1,10 +1,12 @@
 require 'trellohub/form/card'
 require 'trellohub/form/issue'
+require 'trellohub/form/diff'
 
 module Trellohub
   class Form
     include Form::Card
     include Form::Issue
+    include Form::Diff
 
     class << self
       def common_attributes
---
+++ lib/trellohub/form/diff.rb
@@ -0,0 +1,10 @@
+module Trellohub
+  class Form
+    class Diff
+      class << self
+        def compare(base, target)
+        end
+      end
+    end
+  end
+end
