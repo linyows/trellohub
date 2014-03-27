6f4db21ad96cea7299a4fdfc3a59e89930ceefdc
linyows <linyows@gmail.com>
2014-03-27 17:32:42 +0900

    remove Sawyer::Resource from Form.to_hash

--- lib/trellohub/form.rb
+++ lib/trellohub/form.rb
@@ -102,10 +102,8 @@ module Trellohub
 
     def to_hash
       Hash[instance_variables.map { |variable|
+        next if variable.is_a?(Sawyer::Resource)
         variable_no_at = variable.to_s.gsub('@', '')
-
-        next if self.class.origin_attributes.include?(:"#{variable_no_at}")
-
         [variable_no_at.to_sym, instance_variable_get(:"#{variable}")]
       }.compact]
     end
