e64222e4c72e68c34b24576329237ec0ce5bbc09
linyows <linyows@gmail.com>
2014-03-31 00:49:09 +0900

    add .load! spec

--- spec/trellohub_spec.rb
+++ spec/trellohub_spec.rb
@@ -23,4 +23,29 @@ describe Trellohub do
       expect(Trellohub.debug).to eq true
     end
   end
+
+  describe '.load!' do
+    it 'loads config file' do
+      path = './boards/example.yml'
+      expect(YAML).to receive(:load_file).with(path).and_call_original
+      expect(Trellohub::Configurable).to receive(:keys).and_call_original
+      expect(Trellohub::Repository).to receive(:new).with(any_args()).
+        at_least(:once).and_call_original
+      expect(Trellohub::List).to receive(:new).with(any_args()).
+        at_least(:once).and_call_original
+
+      Trellohub.load!(path)
+
+      expect(Trellohub.config_file).to be_nil
+      expect(Trellohub.repositories).to be_a Array
+      expect(Trellohub.repositories.last).to be_a Trellohub::Repository
+      expect(Trellohub.lists).to be_a Array
+      expect(Trellohub.lists.last).to be_a Trellohub::List
+      expect(Trellohub.options).to eq(default_assignee: true, default_member: true)
+      expect(Trellohub.github_api_endpoint).to eq Octokit.api_endpoint
+      expect(Trellohub.github_web_endpoint).to eq Octokit.web_endpoint
+      expect(Trellohub.dry_run).to eq false
+      expect(Trellohub.debug).to eq true
+    end
+  end
 end
